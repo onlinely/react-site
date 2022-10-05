b170aa186dc9e9981138dc6b2bea645de19c63d1
Admin <onlinely@yandex.ru>
2022-10-05 17:51:17 +0300

    edit project

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,3 +1,4 @@
+import {Component} from "react";
 import AppInfo from '../app-info/app-info';
 import SearchPanel from '../search-panel/search-panel';
 import AppFilter from '../app-filter/app-filter';
@@ -6,27 +7,86 @@ import EmployeesAddForm from '../employees-add-form/employees-add-form';
 
 import './app.css';
 
-function App() {
+class App extends Component{
+    constructor(props) {
+        super(props);
+        this.state = {
+            data: [
+                {name: 'Jon C.', salary: 800, increase: false, rise: true, id: 1},
+                {name: 'Alex M.', salary: 300, increase: true, rise: false, id: 2},
+                {name: 'Carl W.', salary: 15000, increase: false, rise: false, id: 3},
+            ]
+        }
+        this.maxId = 4;
+    }
 
-    const data = [
-        {name: 'Jon C.', salary: 800, increase: false, id: 1},
-        {name: 'Alex M.', salary: 300, increase: true, id: 2},
-        {name: 'Carl W.', salary: 15000, increase: false, id: 3},
-    ];
+    deleteItem = (id) => {
+        this.setState(({data}) => {
+            // const index = data.findIndex(elem => elem.id === id);
 
-  return (
-    <div className="app">
-        <AppInfo />
+            // const before = data.slice(0, index);
+            // const after = data.slice(index +1);
+            // const newArr = [...before, ...after]
 
-        <div className="search-panel">
-            <SearchPanel/>
-            <AppFilter/>
-        </div>
-        
-        <EmployeesList data={data}/>
-        <EmployeesAddForm/>
-    </div>
-  );
+
+
+            return {
+                data: data.filter(item => item.id !== id)
+            }
+
+        })
+    }
+
+    addItem = (name, salary) => {
+        const newItem = {
+            name,
+            salary,
+            increase: false,
+            rise: false,
+            id: this.maxId++
+        }
+        this.setState(({data}) => {
+            const newArr = [...data, newItem];
+            return {
+                data: newArr
+            }
+        });
+    }
+
+    onToggleProp = (id, prop) => {
+        this.setState(({data}) => ({
+            data: data.map(item => {
+                if (item.id === id) {
+                    return {...item, [prop]: !item[prop]}
+                }
+                return item;
+            })
+        }))
+    }
+
+
+
+    render() {
+        const employees = this.state.data.length;
+        const icreased= this.state.data.filter( item => item.increase).length;
+        return (
+            <div className="app">
+                <AppInfo employees={employees} icreased={icreased}/>
+
+                <div className="search-panel">
+                    <SearchPanel/>
+                    <AppFilter/>
+                </div>
+
+                <EmployeesList data={this.state.data}
+                               onDelete={this.deleteItem}
+                               onToggleProp={this.onToggleProp}/>
+                <EmployeesAddForm onAdd={this.addItem}/>
+            </div>
+        );
+    }
+
+
 }
 
 export default App;
